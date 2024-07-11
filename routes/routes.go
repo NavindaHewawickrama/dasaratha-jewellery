5e9d645ab764086a0957fdc737e4d8fe261a5b97
Navinda Hewawickrama <124261092+NavindaHewawickrama@users.noreply.github.com>
2024-07-11 07:12:02 +0530

    navinda update 1.1.1

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import(
+	"github.com/NavindaHewawickrama/dasaratha-jewellery/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRouters(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct",controllers.ProductViewverAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}

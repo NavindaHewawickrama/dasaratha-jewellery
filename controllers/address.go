22954495ce26af84a1812e64821c7721810b7f5a
Navinda Hewawickrama <124261092+NavindaHewawickrama@users.noreply.github.com>
2024-08-09 09:05:47 +0530

    Add address

--- controllers/address.go
+++ controllers/address.go
@@ -7,12 +7,71 @@ import (
 
 	"github.com/NavindaHewawickrama/dasaratha-jewellery/models"
 	"github.com/gin-gonic/gin"
+	"gituhb.com/NavindaHewawickrama/dasaratha-jewellery/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func AddAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{
+				"Error": "invalid code",
+			})
+			c.Abort()
+			return
+		}
 
+		address, err:= ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+
+		var addresses models.Address
+
+		addresses.Address_id = primitive.NewObjectID()
+
+		if err := c.ShouldBindJSON(&addresses); err != nil {
+
+
+		address := models.Address{}
+		if err := c.BindJSON(&address); err != nil {
+			c.IndentedJSON(http.StatusNotAcceptable, err.Error())
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		match_filter := bson.D{{Key:"$match",Value: bson.D{primitive.E{Key: "_id",Value: address}}}}
+		unwind := bson.D{{Key: "$unwind",Value: bson.D{primitive.E{Key: "path",Value: "$address"}}}}
+
+
+
+
+
+
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+		update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "address", Value: address}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong command")
+			return
+		}
+
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully Added")
+	}
 }
 
 func EditHomeAddress() gin.HandlerFunc {
